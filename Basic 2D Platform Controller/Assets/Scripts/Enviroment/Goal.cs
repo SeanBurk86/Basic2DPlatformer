8171271522dccdb37a16c2ffd693d7a4a25b54a4
Sean Burk <seanburk86@gmail.com>
2021-03-24 21:43:10 -0500

    Added goal prefab

--- Basic 2D Platform Controller/Assets/Scripts/Enviroment/Goal.cs
+++ Basic 2D Platform Controller/Assets/Scripts/Enviroment/Goal.cs
@@ -4,13 +4,16 @@ using UnityEngine;
 
 public class Goal : MonoBehaviour
 {
+    [SerializeField]
+    private string goalSoundName = "goal";
+
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.transform.parent.GetComponent<Kickable>().isGameBall)
         {
             Destroy(collision.transform.parent.gameObject);
             Debug.Log("Goal");
-            FindObjectOfType<AudioManager>().Play("goal");
+            FindObjectOfType<AudioManager>().Play(goalSoundName);
         }
     }
 
