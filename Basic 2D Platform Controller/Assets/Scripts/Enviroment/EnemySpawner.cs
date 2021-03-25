170f56fb47d7ea00d5fca51c979114f5fc93bdd2
Sean Burk <seanburk86@gmail.com>
2021-03-24 21:12:01 -0500

    Added enemy spawner prefab

---
+++ Basic 2D Platform Controller/Assets/Scripts/Enviroment/EnemySpawner.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySpawner : MonoBehaviour
+{
+    [SerializeField]
+    private Entity enemy;
+
+    [SerializeField]
+    private int capacityOfSpawner = 5;
+
+    [SerializeField]
+    private float spawnWaitTime = 5f;
+
+    private bool needToSpawn;
+    
+    private float lastSpawnTime,
+        lastShortTime;
+
+    public Animator Anim { get; private set; }
+
+    private void Start()
+    {
+        Anim = GetComponent<Animator>();
+    }
+
+    private void Update()
+    {
+        if (CheckSpawnerCapacity())
+        {
+            needToSpawn = false;
+        }
+        else if (!CheckSpawnerCapacity() && !needToSpawn)
+        {
+            lastShortTime = Time.time;
+            needToSpawn = true;
+        }
+        else if (!CheckSpawnerCapacity())
+        {
+            needToSpawn = true;
+        }
+
+        if (needToSpawn)
+        {
+            Anim.SetBool("isSpawning", true);
+            if (Time.time >= lastSpawnTime + spawnWaitTime)
+            {
+                if(Time.time >= lastShortTime + spawnWaitTime)
+                {
+                    SpawnEnemy();
+                    lastSpawnTime = Time.time;
+                    if (CheckSpawnerCapacity())
+                    {
+                        Anim.SetBool("isSpawning", false);
+                    }
+                }
+            }
+        }
+    }
+
+    private void SpawnEnemy()
+    {
+        Instantiate(enemy, transform, false);
+    }
+
+    private bool CheckSpawnerCapacity()
+    {
+        Entity[] spawnedEnemies = GetComponentsInChildren<Entity>();
+        return spawnedEnemies.Length >= capacityOfSpawner;
+    }
+}
