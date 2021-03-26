feae71ca58256ad1e643cc38a6ce00654ed595ad
Sean Burk <seanburk86@gmail.com>
2021-03-25 23:15:18 -0500

    Added pickup dispenser

---
+++ Basic 2D Platform Controller/Assets/Scripts/Enviroment/PickupDispenser.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PickupDispenser : MonoBehaviour
+{
+    [SerializeField]
+    private BulletPickup bulletPickup;
+
+    [SerializeField]
+    private int capacityOfSpawner = 1;
+
+    [SerializeField]
+    private float spawnWaitTime = 5f;
+
+    private bool needToSpawn;
+
+    private float lastSpawnTime,
+        lastShortTime;
+
+    public Animator Anim { get; private set; }
+
+    private void Start()
+    {
+        Anim = GetComponent<Animator>();
+    }
+
+    private void Update()
+    {
+        if (CheckSpawnerCapacity())
+        {
+            needToSpawn = false;
+        }
+        else if (!CheckSpawnerCapacity() && !needToSpawn)
+        {
+            lastShortTime = Time.time;
+            needToSpawn = true;
+        }
+        else if (!CheckSpawnerCapacity())
+        {
+            needToSpawn = true;
+        }
+
+        if (needToSpawn)
+        {
+            Anim.SetBool("isSpawning", true);
+            if (Time.time >= lastSpawnTime + spawnWaitTime)
+            {
+                if (Time.time >= lastShortTime + spawnWaitTime)
+                {
+                    SpawnEnemy();
+                    lastSpawnTime = Time.time;
+                    if (CheckSpawnerCapacity())
+                    {
+                        Anim.SetBool("isSpawning", false);
+                    }
+                }
+            }
+        }
+    }
+
+    private void SpawnEnemy()
+    {
+        Instantiate(bulletPickup, transform, false);
+    }
+
+    private bool CheckSpawnerCapacity()
+    {
+        BulletPickup[] spawnedEnemies = GetComponentsInChildren<BulletPickup>();
+        return spawnedEnemies.Length >= capacityOfSpawner;
+    }
+}
